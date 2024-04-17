4d3e60b8c1c5aee4237a7c41f8415af0b169bc72
fvkd <fvcker@skiff.com>
2024-04-17 05:41:39 -0400

    edited note into comment on 'window manager default'

--- modules/xmonad.nix
+++ modules/xmonad.nix
@@ -11,7 +11,7 @@
         enable = true;
         # Additional XMonad configuration can go here
       };
-      #default = "xmonad"; ## this is throwing error 'The option `xsession.windowManager.default' does not exist.
+      #default = "xmonad"; ## this is throwing error 'The option `xsession.windowManager.default' does not exist. I'm pretty sure that this should be handled in a module or section for whatever current display manager or in xinitrc.
     };
   };
 
