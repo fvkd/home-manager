e12462472175211bb2b0a18370461896e0256498
fvkd <fvcker@skiff.com>
2024-04-17 03:48:44 -0400

    dressed up

--- modules/xmonad.nix
+++ modules/xmonad.nix
@@ -1,1 +1,19 @@
-xsession.windowManager.xmonad.enable = true;
+{
+  config,
+  pkgs,
+  ...
+}: {
+  # Configuration for the X session and XMonad
+  xsession = {
+    enable = true;
+    windowManager = {
+      xmonad = {
+        enable = true;
+        # Additional XMonad configuration can go here
+      };
+      #default = "xmonad";
+    };
+  };
+
+  # Additional Home Manager configuration can go here
+}
